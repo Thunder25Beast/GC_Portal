0955ba4ba7e157c358c2498635b75004afc83006
DeepakSilaych <deepaksilaych@gmail.com>
2024-10-03 20:49:34 +0530

    a

--- frontend/src/pages/instuctions.jsx
+++ frontend/src/pages/instuctions.jsx
@@ -6,11 +6,8 @@ function Instuctions() {
     <div className='int'>
         Instuctions
         <p>
-            Inter IIT Tech Meet is the annual technological competition organized by the Indian Institutes of Technology and qualifies as the only technical event wherein all the IITs participate. This event is hosted in cycle by one of the participating institutions. At end of each Tech Meet the next host IIT is chosen by mutual consensus.
-
-            Tech Meet tries involves students to work on real life problems and pertaining to local issues and areas which require attention but are not extensively address through different platforms. Also it has elements pertaining to knowledge sharing and promoting collaboration among IITs.
-
-            Inter IIT Tech Meet comprises of ardent competitions, exhibitions, illuminating talks and exhilarating pro-nights that inspire, enthrall and mesmerize the massive hordes that attend. According to the pan-IIT Tech Board, “The Inter IIT Tech Meet is a consequence of students of various IITs coming together envisioning a progressive India. We realise that there are countless problems which our country faces due to its population, geographic expanse, climate, infrastructure etc. A lot of efforts are being put by various educational institutes for coming up with technology solutions which could address these issues. Numerous government policies and initiatives have created opportunities for promoting and encouraging the development of technology to address the issues. We feel that that IITs being premier education and research institutes must spearhead these initiatives. Enhanced collaboration between IITs would foster development of technology solutions and also encourage more and more graduating students to pursue this into full time ventures wherein they take this technology from research labs to end users, impacting a million lives.”
+            Tech GC is a year long event that aims to bring together the entire IITB community to participate in a series of competitions.
+            We have a lot of exciting events lined up for you, so make sure you keep an eye out for them!
         </p>
     </div>
   )
