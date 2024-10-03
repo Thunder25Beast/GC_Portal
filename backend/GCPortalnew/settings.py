f1039e4eb8e3aaa0a248f2cdeda78e397050dcb5
DeepakSilaych <deepaksilaych@gmail.com>
2024-10-03 20:18:09 +0530

    a

--- backend/GCPortalnew/settings.py
+++ backend/GCPortalnew/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'scorecard',
     'rest_framework',
     'corsheaders',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     'gunicorn'
 ]
 
@@ -141,6 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
