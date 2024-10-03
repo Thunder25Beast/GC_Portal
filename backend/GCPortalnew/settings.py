626b254a12d6c90fbc1024931943a4e501603a28
DeepakSilaych <deepaksilaych@gmail.com>
2024-10-03 19:44:52 +0530

    a

--- backend/GCPortalnew/settings.py
+++ backend/GCPortalnew/settings.py
@@ -62,11 +62,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
-]
 
 # CORS_ALLOW_METHODS = [
 # 'DELETE',
