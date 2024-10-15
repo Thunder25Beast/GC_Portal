c44b6a9e23415bc0e1757164c71a72682f24adef
DeepakSilaych <deepaksilaych@gmail.com>
2024-10-15 18:44:04 +0530

    a

--- backend/GCPortalnew/settings.py
+++ backend/GCPortalnew/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'scorecard',
     'rest_framework',
     'corsheaders',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'gunicorn'
 ]
 
 REST_FRAMEWORK = {
@@ -159,5 +160,5 @@ DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
